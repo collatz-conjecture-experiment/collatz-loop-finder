65b69c12540d8f8362bc8d24c1c04b389b18337f
collatz-conjecture-experiment <144082712+collatz-conjecture-experiment@users.noreply.github.com>
2023-09-16 20:41:05 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+var number = document.getElementById("number").value;
+var newWindow = window.open("", "_parent");
+newWindow.document.write("<form>");
+for(i = 1; i <= number; i++) {
+    newWindow.document.write('<input type="text" id="box' + i + '" />');
+}
+newWindow.document.write("</form>");
