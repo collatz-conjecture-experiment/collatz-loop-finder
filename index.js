aa039326521cde479196746211c6f0f95e4b5b0b
collatz-conjecture-experiment <144082712+collatz-conjecture-experiment@users.noreply.github.com>
2023-09-16 20:47:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var number = document.getElementById("increases").value;
-var newWindow = window.open("", "_parent");
-newWindow.document.write("<form>");
-for(i = 1; i <= number; i++) {
-    newWindow.document.write('<input type="text" id="box' + i + '" />');
+function increasesSubmitted(){
+    var number = document.getElementById("increases").value;
+    var newWindow = window.open("", "_parent");
+    newWindow.document.write("<form>");
+    for(i = 1; i <= number; i++) {
+        newWindow.document.write('<input type="text" id="box' + i + '" />');
+    }
+    newWindow.document.write("</form>");
 }
-newWindow.document.write("</form>");
